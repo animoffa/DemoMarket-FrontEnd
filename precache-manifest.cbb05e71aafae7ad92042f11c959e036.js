a1d12edf657aeac3826a3856a427ee75c4516236
Anastasia <animoffa@mail.ru>
2020-06-17 17:42:14 +0300

    Updates

---
+++ precache-manifest.cbb05e71aafae7ad92042f11c959e036.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3c7aa0ec236ae55972d31d17c14382",
+    "url": "/DemoMarket-FrontEnd/index.html"
+  },
+  {
+    "revision": "3dd1a45f0ea440e09520",
+    "url": "/DemoMarket-FrontEnd/static/css/main.ff01f4ad.chunk.css"
+  },
+  {
+    "revision": "e52cb7c45cbc0b7505f1",
+    "url": "/DemoMarket-FrontEnd/static/js/2.cdc3b7b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/DemoMarket-FrontEnd/static/js/2.cdc3b7b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dd1a45f0ea440e09520",
+    "url": "/DemoMarket-FrontEnd/static/js/main.c37551f1.chunk.js"
+  },
+  {
+    "revision": "5dd1f49ccb1728fe8b0a",
+    "url": "/DemoMarket-FrontEnd/static/js/runtime-main.6613b259.js"
+  },
+  {
+    "revision": "7151f7bbf0bebd67d414706da6ecb424",
+    "url": "/DemoMarket-FrontEnd/static/media/icons8-меню-50.7151f7bb.png"
+  },
+  {
+    "revision": "3e8039242e517ee7edc05a4e226e0b80",
+    "url": "/DemoMarket-FrontEnd/static/media/loading-37.3e803924.gif"
+  }
+]);
